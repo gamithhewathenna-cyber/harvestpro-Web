89f41400fe529a8f245e0ab9494a6433c373f463
gamithhewathenna-cyber <gamithhewathenna@gmail.com>
2026-09-16 22:07:56 +0530

    css

--- terms-and-conditions.php
+++ terms-and-conditions.php
@@ -26,7 +26,7 @@ $pageImg   = absolute_url('assets/images/hero-bg.jpg');
 <meta name="description" content="<?= e($pageDesc) ?>">
 <?php seo_meta_tags('/terms-and-conditions', $pageTitle, $pageDesc, $pageImg, $brandName . ' Pro'); ?>
 <?php sinhala_font_tags(); ?>
-<link rel="stylesheet" href="assets/css/style.css?v=3.2">
+<link rel="stylesheet" href="assets/css/style.css?v=3.3">
 <?php if ($themePrimary !== '' || $themeAccent !== ''): ?>
 <style>
 :root {
