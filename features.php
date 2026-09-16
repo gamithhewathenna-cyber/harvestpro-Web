f27488356209eb93f5fdd9d27adb47951c29e34a
gamithhewathenna-cyber <gamithhewathenna@gmail.com>
2026-09-16 22:06:35 +0530

    1

--- features.php
+++ features.php
@@ -50,7 +50,7 @@ $pageImg   = absolute_url($fpBanner);
 <?php endif; ?>
 <?php seo_meta_tags('/features', $pageTitle, $pageDesc, $pageImg, $brandName . ' Pro'); ?>
 <?php sinhala_font_tags(); ?>
-<link rel="stylesheet" href="assets/css/style.css?v=3.1">
+<link rel="stylesheet" href="assets/css/style.css?v=3.2">
 <?php if ($themePrimary !== '' || $themeAccent !== ''): ?>
 <style>
 :root {
