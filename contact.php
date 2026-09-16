73f1fd6ec46c3b2c61257030cdedb2f4e9397bbb
gamithhewathenna-cyber <gamithhewathenna@gmail.com>
2026-09-16 22:56:28 +0530

    1

--- contact.php
+++ contact.php
@@ -44,7 +44,7 @@ $pageImg   = absolute_url($cBannerBg);
 <?php endif; ?>
 <?php seo_meta_tags('/contact', $pageTitle, $pageDesc, $pageImg, $brandName . ' Pro'); ?>
 <?php sinhala_font_tags(); ?>
-<link rel="stylesheet" href="assets/css/style.css?v=4.0">
+<link rel="stylesheet" href="assets/css/style.css?v=4.1">
 <?php if ($themePrimary !== '' || $themeAccent !== ''): ?>
 <style>
 :root {
--- features.php
+++ features.php
@@ -50,7 +50,7 @@ $pageImg   = absolute_url($fpBanner);
 <?php endif; ?>
 <?php seo_meta_tags('/features', $pageTitle, $pageDesc, $pageImg, $brandName . ' Pro'); ?>
 <?php sinhala_font_tags(); ?>
-<link rel="stylesheet" href="assets/css/style.css?v=4.0">
+<link rel="stylesheet" href="assets/css/style.css?v=4.1">
 <?php if ($themePrimary !== '' || $themeAccent !== ''): ?>
 <style>
 :root {
