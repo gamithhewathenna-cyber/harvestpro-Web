6f8fee3905014787cbfbf13dde5e88ebd812d894
gamithhewathenna-cyber <gamithhewathenna@gmail.com>
2026-09-16 22:04:37 +0530

    1

--- refund-policy.php
+++ refund-policy.php
@@ -26,7 +26,7 @@ $pageImg   = absolute_url('assets/images/hero-bg.jpg');
 <meta name="description" content="<?= e($pageDesc) ?>">
 <?php seo_meta_tags('/refund-policy', $pageTitle, $pageDesc, $pageImg, $brandName . ' Pro'); ?>
 <?php sinhala_font_tags(); ?>
-<link rel="stylesheet" href="assets/css/style.css?v=3.0">
+<link rel="stylesheet" href="assets/css/style.css?v=3.1">
 <?php if ($themePrimary !== '' || $themeAccent !== ''): ?>
 <style>
 :root {
